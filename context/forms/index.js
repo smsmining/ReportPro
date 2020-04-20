7d05dc40fd4a3983725630b83b7d2a9ced101fb7
smsdodwyer <51978986+smsdodwyer@users.noreply.github.com>
2020-04-20 09:47:09 +0800

    AppFlag.DevBuild (#120)

--- context/forms/index.js
+++ context/forms/index.js
@@ -1,3 +1,5 @@
+import AppFlags from '../../AppFlags';
+
 import Hazard_Report from './Hazard_Report';
 import Inspection_Report from './Inspection_Report';
 import SMS_MN_INS_0004_01 from './SMS-MN-INS-0004_01';
@@ -11,6 +13,7 @@ import SMS_SAF_FRM_0014_02 from './SMS-SAF-FRM-0014_02';
 import Take_5 from './Take_5';
 import Workshop_Inspection from './Workshop_Inspection';
 
+
 export default Forms =
     [Take_5
     ,Hazard_Report
@@ -18,10 +21,15 @@ export default Forms =
     ,Workshop_Inspection
     ,SMS_SAF_FRM_0014_02
     ,SMS_SAF_FRM_0013_03
-    ,SMS_MN_INS_0004_01
+    
+    ,...(AppFlags.DevBuild
+?   [SMS_MN_INS_0004_01
     ,SMS_MN_INS_0005_01
     ,SMS_MN_INS_0006_01
     ,SMS_MN_INS_0007_01
     ,SMS_MN_INS_0008_01
     ,SMS_MN_INS_0009_01
+    ]
+:   []
+    )
     ]
